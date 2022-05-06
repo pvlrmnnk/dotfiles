c417d605f7478815c3460500397c2add54564bcb
Pavlo Romanenko <mail@pvlrmnnk.com>
2022-05-06 12:20:34 +0300

    Update zsh init
    - Cleanup

--- dot_config/zsh/init.zsh
+++ dot_config/zsh/init.zsh
@@ -5,24 +5,6 @@ export PATH="/usr/local/bin:$PATH"
 # gpg
 export GPG_TTY="$(tty)"
 
-alias ls="ls -G"
-alias ll="exa -aFgl --time-style long-iso --group-directories-first"
-
-b() {
-    if [[ $@ == "o" ]]; then
-        brew update > /dev/null 2>&1 \
-        && brew outdated --greedy
-    elif [[ $@ == "u" ]]; then
-        command brew upgrade --greedy
-    else
-        command brew "$@"
-    fi
-}
-
-s() {
-    source ~/.zshrc
-}
-
 autoload -Uz compinit && compinit -i
 
 alias cz="chezmoi"
