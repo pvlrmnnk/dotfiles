c4b7d9f67f0299c19973da555980131ed3ea2ed2
Pavlo Romanenko <mail@pvlrmnnk.com>
2022-05-06 12:21:24 +0300

    Add chezmoi
    - All aliases

---
+++ dot_config/zsh/conf.d/30-chezmoi.zsh
@@ -0,0 +1,5 @@
+alias cz="chezmoi"     # CheZmoi
+alias czm="cz managed" # CheZmoi Managed
+alias cza="cz add"     # CheZmoi Add
+alias czp="cz apply"   # CheZmoi aPply
+alias cze="cz edit"    # CheZmoi Edit
