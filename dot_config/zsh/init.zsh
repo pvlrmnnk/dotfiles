0c737e4a52f700d00b59ed051230c83b8c426314
Pavlo Romanenko <mail@pvlrmnnk.com>
2022-05-10 20:42:07 +0300

    Update zsh config
    - Init cleanup
    - Add brew config

--- dot_config/zsh/init.zsh
+++ dot_config/zsh/init.zsh
@@ -1,13 +1,7 @@
-# brew
-HOMEBREW_NO_AUTO_UPDATE=1
-export PATH="/usr/local/bin:$PATH"
-
 # gpg
 export GPG_TTY="$(tty)"
 
 autoload -Uz compinit && compinit -i
-
-alias cz="chezmoi"
 
 # for zshrc in $ZSH_CONFIG_DIR/rc.d/**/*.zsh; do
 for conf in $HOME/.config/zsh/conf.d/**/*.zsh; do
