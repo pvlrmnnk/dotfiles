897036826b605779ff98e9d11c9f14fb7980443d
Pavlo Romanenko <mail@pvlrmnnk.com>
2022-05-11 11:13:37 +0300

    Update zsh config
    - Change weird error messages from unalias

--- dot_config/zsh/conf.d/10-shell.zsh
+++ dot_config/zsh/conf.d/10-shell.zsh
@@ -1,6 +1,5 @@
-# remove weird aliases
-unalias run-help
-unalias which-command
+unalias run-help >/dev/null 2>&1
+unalias which-command >/dev/null 2>&1
 
 alias za="alias"               # Zsh Alias
 alias zs="source $HOME/.zshrc" # Zsh Source
