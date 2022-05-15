f9ee831ee4ebc9fd8a117c93f7e37d06e49dbc4c
Pavlo Romanenko <mail@pvlrmnnk.com>
2022-05-15 14:03:25 +0300

    Add brew aliases

--- dot_config/zsh/conf.d/20-brew.zsh
+++ dot_config/zsh/conf.d/20-brew.zsh
@@ -8,3 +8,24 @@ export HOMEBREW_CLEANUP_MAX_AGE_DAYS=30
 export HOMEBREW_UPDATE_REPORT_ONLY_INSTALLED=1
 
 export PATH="/usr/local/bin:$PATH"
+
+# BreW
+alias bw="brew"
+# UPdate
+alias bwup="brew update"
+# UpgraDe
+alias bwud="brew upgrade"
+# Outdated
+alias bwo="brew outdated"
+# Search
+alias bws="brew search"
+# Install
+alias bwi="brew install"
+# Uninstall
+alias bwu="brew uninstall"
+# Cleanup
+alias bwc="brew autoremove && brew cleanup"
+# List
+alias bwl="brew list"
+# inFo
+alias bwf="brew info"
