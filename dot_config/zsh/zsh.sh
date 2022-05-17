ea6f3c7865e44595602ee34a9a3b3923144e5122
Pavlo Romanenko <mail@pvlrmnnk.com>
2022-05-17 23:38:57 +0300

    Add zsh history config

--- dot_config/zsh/zsh.sh
+++ dot_config/zsh/zsh.sh
@@ -6,11 +6,18 @@ export PATH="${BIN_DIR}:${PATH}"
 
 export ZSH_CONFIG_DIR="${CONFIG_DIR}/zsh"
 export ZSH_SHARE_DIR="${SHARE_DIR}/zsh"
-alias za="alias" # Zsh Alias
-alias zs="source $HOME/.zshrc" # Zsh Source
+export HISTFILESIZE=1000000
+export HISTSIZE=1000000
+export HISTFILE="${HOME}/.zsh_history"
+
+setopt INC_APPEND_HISTORY
+
 unalias run-help >/dev/null 2>&1
 unalias which-command >/dev/null 2>&1
+alias za="alias" # Zsh Alias
+alias zs="source $HOME/.zshrc" # Zsh Source
 
+# etc
 source "${ZSH_CONFIG_DIR}/brew.sh"
 source "${ZSH_CONFIG_DIR}/ls.sh"
 source "${ZSH_CONFIG_DIR}/gpg.sh"
@@ -18,4 +25,5 @@ source "${ZSH_CONFIG_DIR}/chezmoi.sh"
 source "${ZSH_CONFIG_DIR}/vscode.sh"
 source "${ZSH_CONFIG_DIR}/go.sh"
 
+# prompt
 eval "$(starship init zsh)"
